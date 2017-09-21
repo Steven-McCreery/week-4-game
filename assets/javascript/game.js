3a88753f17e17e7907034444fd6fca88c211b780
Steven McCreery <logad8@gmail.com>
2017-09-21 01:49:20 -0400

    changed html and css. js can now move all character elements properly on first click event but not second.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,64 @@
-window.onload = function() {
-
+$(document).ready(function() {
 	$(".fighter").on("click", fighterSelect);
-	$(".fighter").on("click", defenderSelect);
+	$(".other").on("click", enemySelect);
 	$(".begin").on("click", battle);
-
-
-
-
-
-
-
-}
+})
 
 
 var fighter = false;
 
-var defender = false;
+var enemy = false;
 
-var defeated = [];
+var fighterSelect;
 
-var selector;
+var enemySelect;
 
 var battle;
 
+function Character(powerIncrement, power, defense, health, available) {
+	this.powerIncrement = powerIncrement;
+	this.power = power;
+	this.defense = defense;
+	this.health = health;
+	this.available = available;
+}
+
+var character1 = new Character(111, 111, 111, 111, true);
+
+var character2 = new Character(222, 222, 222, 222, true);
+
+var character3 = new Character(333, 333, 333, 333, true);
+
+var character4 = new Character(444, 444, 444, 444, true);
 
 
 fighterSelect = function() {
 	if (fighter == false) {
-		$($(this)).appendTo(".attacker");
+		$($(this)).appendTo(".battle");
 		fighter = true;
+		$($(this)).removeClass("fighter");
+		$($(this)).addClass("main");
+		$(".fighter").addClass("other");
+		$(".fighter").removeClass("fighter");
+		$(".other").appendTo(".defenders");
 	}
 }
 
-defenderSelect = function() {
-	if (defender == true) {
+enemySelect = function() {
+	if (enemy == true) {
+		setTimeout(function(){
 		alert("You have already selected a defender, you must first battle!");
+		},1000 * .2)
 	}
 
-	if (defender = false && fighter == true) {
-		$($(this)).appendTo(".defender");
-		defender = true;
+	if (enemy == false) {
+		$($(this)).appendTo(".battle");
+		enemy = true;
+		$($(this)).removeClass("other");
+		$($(this)).addClass("enemy");
+		console.log("enemy selected");
 	}
+
+
 }
 
