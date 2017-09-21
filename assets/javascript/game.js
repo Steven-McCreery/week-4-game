2d5e6156369ea4c91b665e05befdb4fc6804acf8
Steven McCreery <logad8@gmail.com>
2017-09-21 03:48:32 -0400

    trying different methods of making the second move happen, no success

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,11 @@
+
 $(document).ready(function() {
 	$(".fighter").on("click", fighterSelect);
-	$(".other").on("click", enemySelect);
+	$(".defenders.other").on("click", enemySelect);
+	// $(document).on("click", "other", enemySelect);
+	// $("button:not(.fighter)").on("click", enemySelect);
 	$(".begin").on("click", battle);
+
 })
 
 
@@ -15,21 +19,73 @@ var enemySelect;
 
 var battle;
 
-function Character(powerIncrement, power, defense, health, available) {
+// var clicks = 0;
+
+//object constructor for characters
+function Character(powerIncrement, power, defense, health, available, currentAttacker, currentDefender) {
 	this.powerIncrement = powerIncrement;
 	this.power = power;
 	this.defense = defense;
 	this.health = health;
 	this.available = available;
+	this.currentAttacker = currentAttacker;
+	this.currentDefender = currentDefender;
 }
 
-var character1 = new Character(111, 111, 111, 111, true);
+var character1 = new Character(111, 111, 111, 111, true, false, false);
 
-var character2 = new Character(222, 222, 222, 222, true);
+var character2 = new Character(222, 222, 222, 222, true, false, false);
 
-var character3 = new Character(333, 333, 333, 333, true);
+var character3 = new Character(333, 333, 333, 333, true, false, false);
 
-var character4 = new Character(444, 444, 444, 444, true);
+var character4 = new Character(444, 444, 444, 444, true, false, false);
+
+$("#fighter1").data(character1);
+
+$("#fighter2").data(character2);
+
+$("#fighter3").data(character3);
+
+$("#fighter4").data(character4);
+
+
+
+
+// fighterSelect = function() {
+// 	clicks = 0;
+
+// 	if (clicks == 0){
+// 		if (fighter == false) {
+// 			$($(this)).appendTo(".battle");
+// 			fighter = true;
+// 			$($(this)).removeClass("fighter");
+// 			$($(this)).addClass("main");
+// 			$(this).attr("currentAttacker", true);
+// 			$(".fighter").addClass("other");
+// 			// $(".fighter").removeClass("fighter");
+// 			$(".other").appendTo(".defenders");
+// 			clicks = 1;
+// 		}
+// 	}
+// 	else {
+// 		if (enemy == true) {
+// 			setTimeout(function(){
+// 			alert("You have already selected a defender, you must first battle!");
+// 			},1000 * .2)
+// 		}
+
+// 		if (enemy == false) {
+// 			$($(this)).appendTo(".battle");
+// 			enemy = true;
+// 			$($(this)).removeClass("other");
+// 			$($(this)).removeClass("fighter");
+// 			$($(this)).addClass("enemy");
+// 			console.log("enemy selected");
+// 		}
+// 	}
+
+// }
+
 
 
 fighterSelect = function() {
@@ -38,8 +94,9 @@ fighterSelect = function() {
 		fighter = true;
 		$($(this)).removeClass("fighter");
 		$($(this)).addClass("main");
+		$(this).attr("currentAttacker", true);
 		$(".fighter").addClass("other");
-		$(".fighter").removeClass("fighter");
+		// $(".fighter").removeClass("fighter");
 		$(".other").appendTo(".defenders");
 	}
 }
@@ -55,10 +112,29 @@ enemySelect = function() {
 		$($(this)).appendTo(".battle");
 		enemy = true;
 		$($(this)).removeClass("other");
+		$($(this)).removeClass("fighter");
 		$($(this)).addClass("enemy");
 		console.log("enemy selected");
 	}
-
-
 }
+
+
+
+// fighterSelect();
+
+
+// enemySelect();
+
+// battle = function() {
+// 	if (fighter == true && enemy == true) {
+// 		$(".begin").on("click", function(){
+// 			if ($(".enemy").health < 1) {.
+
+// 			}
+// 		})
+// 	}
+// }
+
+
+
 
