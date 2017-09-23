d87521feae0e626fa2db5c3ddc89fc6e22ffff35
Steven McCreery <logad8@gmail.com>
2017-09-22 22:32:51 -0400

    can move object into fighter, defender, enemy, and defeated positions.  Increments not working, defeated enemy can be reselected.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
+
 //ready when loading is complete
 $(document).ready(function() {
 	$(".fighter").on("click", fighterSelect);
-	$(".defenders.other").on("click", enemySelect);
-	// $(document).on("click", "other", enemySelect);
+		// $(document).on("click", "other", enemySelect);
 	// $("button:not(.fighter)").on("click", enemySelect);
 	$(".begin").on("click", battle);
 
@@ -11,7 +11,7 @@ $(document).ready(function() {
 //defining initial variables
 var fighter = false;
 
-var enemy = false;
+var isEnemy = false;
 
 var fighterSelect;
 
@@ -23,33 +23,37 @@ var battle;
 
 //object constructor for characters
 function Character(powerIncrement, power, defense, health, available, currentAttacker, currentDefender) {
-	this.powerIncrement = powerIncrement;
-	this.power = power;
-	this.defense = defense;
-	this.health = health;
-	this.available = available;
-	this.currentAttacker = currentAttacker;
-	this.currentDefender = currentDefender;
-}
+	return{
+		"powerIncrement":powerIncrement,
+		"power":power,
+		"defense":defense,
+		"health":health,
+		"available":available,
+		"currentAttacker":currentAttacker,
+		"currentDefender":currentDefender,
+		}
+	}
+	
 
 //creating 4 new objects with above template
-var character1 = new Character(111, 111, 111, 111, true, false, false);
+var character1 = Character(111, 111, 111, 111, true, false, false);
 
-var character2 = new Character(222, 222, 222, 222, true, false, false);
+var character2 = Character(222, 222, 222, 222, true, false, false);
 
-var character3 = new Character(333, 333, 333, 333, true, false, false);
+var character3 = Character(333, 333, 333, 333, true, false, false);
 
-var character4 = new Character(444, 444, 444, 444, true, false, false);
+var character4 = Character(444, 444, 444, 444, true, false, false);
+
+console.log(character1);
+
+var fighter2Character = {
+	"fighter1":character1,
+	"fighter2":character2,
+	"fighter3":character3,
+	"fighter4":character4,
+	}
 
 //adding the newly created objects to the html elements
-$("#fighter1").data(character1);
-
-$("#fighter2").data(character2);
-
-$("#fighter3").data(character3);
-
-$("#fighter4").data(character4);
-
 
 
 //trying to make object move in as a challenger to first selected with a click tracker
@@ -103,11 +107,12 @@ fighterSelect = function() {
 		fighter = true;
 		$($(this)).removeClass("fighter");
 		$($(this)).addClass("main");
-		$($(this)).data("currentAttacker", true);
-		$($(this)).data("available", false);
+		// $($(this)).data("currentAttacker", true);
+		// $($(this)).data("available", false);
 		$(".fighter").addClass("other");
 		$(".fighter").removeClass("fighter");
 		$(".other").appendTo(".defenders");
+		$(".other").on("click", enemySelect);
 	}
 }
 
@@ -115,23 +120,23 @@ fighterSelect = function() {
 enemySelect = function() {
 
 	//alerting user of enemy selected as a filled condition
-	if (enemy == true) {
+	if (isEnemy == true) {
 		setTimeout(function(){
 		alert("You have already selected a defender, you must first battle!");
 		},1000 * .2)
 	}
 
 	//if there is no enemy, then the clicked on item is moved into the enemy position, adjusting classes and object property values
-	if (enemy == false) {
+	if (isEnemy == false) {
 		$($(this)).appendTo(".battle");
-		enemy = true;
+		isEnemy = true;
 		$($(this)).removeClass("other");
 		$($(this)).removeClass("fighter");
 		$($(this)).addClass("enemy");
-		$($(this)).data("available", false);
-		$($(this)).data("currentDefender", true);
-		$(".begin").removeClass(".inactive");
-		$(".begin").addClass(".active");
+		// $($(this)).data("available", false);
+		// $($(this)).data("currentDefender", true);
+		$(".begin").removeClass("inactive");
+		$(".begin").addClass("active");
 		console.log("enemy selected");
 	}
 }
@@ -140,53 +145,66 @@ enemySelect = function() {
 
 //combat mechanic on button click when an attacker and defender are present
 battle = function() {
-	if (fighter == true && enemy == true) {
-		$(".begin").on("click", function() {
+	// if (fighter === true && enemy === true) {
+	var main = fighter2Character[$(".main").attr('id')];
+	var enemy = fighter2Character[$(".enemy").attr('id')];
+	console.log(main);
+	console.log(enemy);
+	var mainHealth = main.health;
+	var mainPower = main.power;
+	var mainInc = main.powerIncrement;
+	var enemyHealth = enemy.health;
+	var enemyDefense = enemy.defense;
 
-			//enemy's health less the main character's attack value
-			$(".enemy").data(health) - $(".main").data(power) = $(".enemy").data(health);
+	//enemy's health less the main character's attack value
+	$(enemy).data("health", enemyHealth - mainPower);
+	$(".enemy .health").html($(".enemy").data("health"));
+	console.log($(enemy).data("health"));
 
-			//main charater's power increases
-			$(".main").data(power) + $(".main").data(powerIncrement) = $(".main").data(power);
+	//main charater's power increases
+	$(main).data("power", $(main).data("power") + $(main).data("powerIncrement"));
+	// $(".main .power").html($(".main").data("power"));
 
-			//reducing main character's health by enemy defense
-			$(".main").data(health) - $(".enemy").data(defense) = $(".main").data(health);
+	//reducing main character's health by enemy defense
+	$(main).data("health", (mainHealth - enemyDefense));
+	$(".main .health").html($(".main").data("health"));
 
-			//defeated enemy conditions
-			if ($(".enemy").health < 1) {
-				$(".enemy").appendTo(".defeated");
-				setTimeout(function(){
-				alert("You have defeated the challenger!  Select another enemy.");
-				},1000 * .2)
+	//defeated enemy conditions
+	if ($(enemy).data("health") < 1) {
+		$(".enemy").appendTo(".defeated");
+		$(".enemy").removeClass("enemy");
+		$(".begin").addClass("defeated");
+		setTimeout(function(){
+		alert("You have defeated the challenger!  Select another enemy.");
+		},1000 * .2)
 
-				//changing statuses now that enemy is defeated
-				$(".enemy").data("available", false);
-				$(".enemy").data("currentDefender", false);
-				enemy = false;
+		//changing statuses now that enemy is defeated
+		isEnemy = false;
+		// $(".enemy").data("available", false);
+		// $(".enemy").data("currentDefender", false);
 
-				//changing battle button to inactive
-				$(".begin").removeClass(".active");
-				$(".begin").addClass(".inactive"); 
+		//changing battle button to inactive
+		$(".begin").removeClass(".active");
+		$(".begin").addClass(".inactive"); 
 
-				//incrementing wins and checking if winning condition met
-				wins = wins + 1;
-				if (wins = 3) {
-					alert("You have defeated all the challengers!  Well done!");
-				}
-			}
-
-			//checking for main character's health for losing condition
-			if ($(".main").health < 1) {
-				setTimeout(function(){
-				alert("You have been bested by your enemies!  Reaload the page to retry.");
-				},1000 * .2)
-
-				//inactivating battle button on loss
-				$(".begin").removeClass(".active");
-				$(".begin").addClass(".inactive");
-			}
-		})
+		//incrementing wins and checking if winning condition met
+		wins = wins + 1;
+		if (wins = 3) {
+			alert("You have defeated all the challengers!  Well done!");
+		}
 	}
+
+	//checking for main character's health for losing condition
+	if ($(".main").data("health") < 1) {
+		setTimeout(function(){
+		alert("You have been bested by your enemies!  Reaload the page to retry.");
+		},1000 * .2)
+
+		//inactivating battle button on loss
+		$(".begin").removeClass(".active");
+		$(".begin").addClass(".inactive");
+	}
+	
 }
 
 
